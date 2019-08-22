bf49c3df2d154f2664bb51baff5944dd28aa0676
alexddhuang <alexddhuang@gmail.com>
2019-08-22 14:09:34 +0800

    CharacterController and CameraFollower

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterController : MonoBehaviour
+{
+    private NavMeshAgent mMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                GameObject hitObject = hit.transform.gameObject;
+                Brick brick = hitObject.GetComponent<Brick>();
+                if (brick != null) {
+                    mMeshAgent.SetDestination(hit.transform.position);
+                }
+            }
+        }
+    }
+}
