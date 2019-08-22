b06202bb7d35da9e3efcfecd922c05683ff726da
alexddhuang <alexddhuang@gmail.com>
2019-08-22 16:41:56 +0800

    ShowSecret

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -1,24 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public enum BrickState {
-    Unknown,
-    Known,
-}
+using UnityEditor;
 
 public class Brick : MonoBehaviour
 {
+    private static Dictionary<string, Sprite> mTileImages;
+
     public bool mine = false;
 
     public float radius = 1.42f;
 
+    public SpriteRenderer tile = null;
+
     private List<Brick> mNeighbors;
+
+    public static void BuildSpritesMap()
+    {
+        if (mTileImages == null) {
+            Object[] sprites = AssetDatabase.LoadAllAssetRepresentationsAtPath("Textures/MinesweeperSpritesheet.png");
+            mTileImages = new Dictionary<string, Sprite>();
+            for (int i = 0; i < sprites.Length; i++) {
+                mTileImages.Add(sprites[i].name, (Sprite) sprites[i]);
+            }
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         FindNeighbors();
+        BuildSpritesMap();
     }
 
     // Update is called once per frame
@@ -42,5 +54,24 @@ public class Brick : MonoBehaviour
         }
 
         Debug.Log($"{mNeighbors.Count} neighbors");
+    }
+
+    public void ShowSecret()
+    {
+        string name;
+
+        if (mine) {
+            name = "TileMine";
+        } else {
+            int num = 0;
+            mNeighbors.ForEach(brick => {
+                if (brick.mine) num += 1;
+            });
+            name = $"Tile{num}";
+        }
+
+        Sprite sprite;
+        if (mTileImages.TryGetValue(name, out sprite))
+            tile.sprite = sprite;
     }
 }
